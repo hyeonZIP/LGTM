a79b40ea42e4b6e82e0a98824b86236d23080d66
hyeonZIP <aminez4466@gmail.com>
2026-01-11 21:45:38 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/zip/hyeon/lgtm/LgtmApplication.java
@@ -0,0 +1,13 @@
+package zip.hyeon.lgtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgtmApplication {
+
+    void main() {
+        SpringApplication.run(LgtmApplication.class);
+    }
+
+}
