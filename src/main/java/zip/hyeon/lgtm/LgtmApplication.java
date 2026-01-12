9008ae3fab0efc61e7dc46757e93b505a223633e
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2026-01-12 14:57:26 +0900

    feat: 생성,수정일 필드를 위한 기능 활성화

--- src/main/java/zip/hyeon/lgtm/LgtmApplication.java
+++ src/main/java/zip/hyeon/lgtm/LgtmApplication.java
@@ -2,7 +2,9 @@ package zip.hyeon.lgtm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class LgtmApplication {
 
