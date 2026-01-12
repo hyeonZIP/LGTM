ea4b252e63d7b33ad3c2bf1f2ef32b684127dc86
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2026-01-12 14:18:42 +0900

    feat: Entity 중복 필드 추상화
    AbstractEntity 테이블 PK 필드
    AbstractTimeEntity PK 필드 상속 및 생성일, 수정일 필드

---
+++ src/main/java/zip/hyeon/lgtm/domain/AbstractTimeEntity.java
@@ -0,0 +1,23 @@
+package zip.hyeon.lgtm.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractTimeEntity extends AbstractEntity{
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
