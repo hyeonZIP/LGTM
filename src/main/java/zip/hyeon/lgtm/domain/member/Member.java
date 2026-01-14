954ec7cab86d5bd2347dfc8f6650aa0968bb8e7a
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2026-01-14 17:58:27 +0900

    feat: 회원가입 및 업데이트 메서드 추가
    프로필 이미지 NotNull 제약조건 추가

--- src/main/java/zip/hyeon/lgtm/domain/member/Member.java
+++ src/main/java/zip/hyeon/lgtm/domain/member/Member.java
@@ -1,5 +1,7 @@
 package zip.hyeon.lgtm.domain.member;
 
+import static java.util.Objects.requireNonNull;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,15 +9,18 @@ import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import zip.hyeon.lgtm.application.member.dto.MemberRegisterRequest;
 import zip.hyeon.lgtm.domain.AbstractTimeEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends AbstractTimeEntity {
+
     @Column(nullable = false)
     private String username;
 
+    @Column(nullable = false)
     private String profileImageUrl;
 
     @Enumerated(EnumType.STRING)
@@ -25,4 +30,20 @@ public class Member extends AbstractTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status;
+
+    public static Member register(MemberRegisterRequest request) {
+        Member member = new Member();
+
+        member.username = requireNonNull(request.username());
+        member.profileImageUrl = requireNonNull(request.profileImageUrl());
+        member.role = Role.MEMBER;
+        member.status = Status.ACTIVATED;
+
+        return member;
+    }
+
+    public void update(MemberRegisterRequest request) {
+        this.username = requireNonNull(request.username());
+        this.profileImageUrl = requireNonNull(request.profileImageUrl());
+    }
 }
