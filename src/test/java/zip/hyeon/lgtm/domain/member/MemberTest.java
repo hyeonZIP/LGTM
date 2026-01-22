80b25643219b1b1c864b3871db707587a09d01f0
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2026-01-22 18:21:30 +0900

    chore: @DisplayName 제거

--- src/test/java/zip/hyeon/lgtm/domain/member/MemberTest.java
+++ src/test/java/zip/hyeon/lgtm/domain/member/MemberTest.java
@@ -3,10 +3,7 @@ package zip.hyeon.lgtm.domain.member;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import zip.hyeon.lgtm.application.member.dto.MemberRegisterRequest;
-import zip.hyeon.lgtm.application.member.dto.MemberUpdateRequest;
 
 class MemberTest {
 
@@ -18,14 +15,12 @@ class MemberTest {
     }
 
     @Test
-    @DisplayName("등록된 회원의 기본값은 활성화 및 일반 회원이다")
     void registerMember() {
         assertThat(member.getStatus()).isEqualTo(Status.ACTIVATED);
         assertThat(member.getRole()).isEqualTo(Role.MEMBER);
     }
 
     @Test
-    @DisplayName("회원 정보를 업데이트 한다")
     void updateMember() {
         var updateRequest = new MemberUpdateRequest("newName", "newProfile.png");
 
